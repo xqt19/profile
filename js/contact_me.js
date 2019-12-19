ebc0b5840d7f836c01e285af1f49b1fb268be67d
XQT19 <tan.xiu.qi.11@gmail.com>
2019-12-19 14:39:10 +0800

    animations now recurr

--- js/contact_me.js
+++ js/contact_me.js
@@ -73,3 +73,59 @@ $(function() {
 $('#name').focus(function() {
   $('#success').html('');
 });
+
+
+
+
+// Adding more stuff
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.col-md-6');
+var mastheadavatar = document.querySelectorAll('.masthead-avatar');
+
+function loop() {
+
+    Array.prototype.forEach.call(mastheadavatar, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('bounceInRight');
+      } else {
+        element.classList.remove('bounceInRight');
+      }
+    });
+
+    Array.prototype.forEach.call(elementsToShow, function(element){
+      if (isElementInViewport(element)) {
+        element.classList.add('animated');
+        element.classList.add('bounceIn');
+      } else {
+        element.classList.remove('animated');
+        element.classList.remove('bounceIn');
+      }
+    });
+
+    scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
